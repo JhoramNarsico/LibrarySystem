2a1863bda09a50567febb73c7ef8a4f0b7010cad
JhoramNarsico <jhoramnarsico@gmail.com>
2025-05-08 18:04:39 +0800

    minor revisions

--- MyLibraryApp/src/main/java/Main.java
+++ MyLibraryApp/src/main/java/Main.java
@@ -6,14 +6,12 @@ import ui.LoginFrame;
 public class Main {
     @SuppressWarnings("UseSpecificCatch")
     public static void main(String[] args) {
-        // Set UIManager properties early for a better look and feel.
+        
         try {
-            // Use the system's native look and feel
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             System.err.println("Couldn't set system look and feel. Using default.");
         }
-
         SwingUtilities.invokeLater(() -> {
             new LoginFrame().setVisible(true);
         });
